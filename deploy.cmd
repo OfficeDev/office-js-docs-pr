c66d4c1d35b985caac7ad48d3dc54f4656110e84
jinlinxu <jnlxu@microsoft.com>
2016-04-21 14:02:24 -0700

    Update the converter folder name.

--- deploy.cmd
+++ deploy.cmd
@@ -16,11 +16,11 @@ IF NOT DEFINED DEPLOYMENT_TARGET_DIR (
 )
 
 IF NOT DEFINED DEPLOYMENT_TEMPLATE (
-  SET DEPLOYMENT_TEMPLATE=%SITE%\wwwroot\MDConvert\html-template
+  SET DEPLOYMENT_TEMPLATE=%SITE%\wwwroot\MDConverter\html-template
 )
 
 IF NOT DEFINED APIDOCS_PATH (
-  SET APIDOCS_PATH=%SITE%\wwwroot\MDConvert\bin
+  SET APIDOCS_PATH=%SITE%\wwwroot\MDConverter\bin
 )
 
 %APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\docs --output %DEPLOYMENT_TARGET_DIR%\docs --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/office-js-docs/tree/master
